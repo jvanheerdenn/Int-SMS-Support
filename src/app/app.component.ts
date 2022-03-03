56957b0603910127616c9378400f758152de7059
jvanheerdenn <jvanheerden@vectoritcgroup.com>
2022-03-03 09:07:16 +0100

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  telephoneNumber = '14056584895';
+  telephoneNumber: string;
+  inputs = [
+    {
+      name: 'input1',
+      value: '14056584895'
+    },
+    {
+      name: 'input2',
+      value: '01154 2974134076'
+    },
+    {
+      name: 'input3',
+      value: '011355042273778'
+    },
+  ]
+
+  changeValue(event){
+    this.telephoneNumber = event.value;
+  }
 }
--- src/app/international-number-formatter.pipe.ts
+++ src/app/international-number-formatter.pipe.ts
@@ -9,18 +9,14 @@ export class InternationalNumberFormatterPipe implements PipeTransform {
   private readonly libraryInstance = PhoneNumberUtil.getInstance();
   transform(value: string): string | null {
     if (!value) return null;
-    let data = flagsNameCountryCode;
     const hasPlus = value.slice(0, 3) === '011' ? true : false;
-    console.log(hasPlus);
-    const numberWithoutPlus = value.slice(3);
     if (hasPlus) {
-      let countryPrefix: number;
-      if (value !== value.replace(/  /g, ' ')) {
-        countryPrefix = parseInt(value.slice(3, 4));
-      } else {
-        countryPrefix = parseInt(value.slice(3, 6));
-      }
-      const countryData = data.find((el) => el.countryPrefix === countryPrefix);
+      const numberWithoutPlus = value.slice(3);
+      const countryPrefix =
+        value !== value.replace(/  /g, ' ')
+          ? parseInt(value.slice(3, 4))
+          : parseInt(value.slice(3, 6));
+      const countryData = flagsNameCountryCode.find((el) => el.countryPrefix === countryPrefix);
       const number = this.libraryInstance.parseAndKeepRawInput(
         numberWithoutPlus,
         countryData.regionCode
